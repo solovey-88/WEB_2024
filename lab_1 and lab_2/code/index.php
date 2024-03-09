d31a8dab33473d432d8ecf8ff25f3c2a7c04a361
solovey-88 <131691574+solovey-88@users.noreply.github.com>
2024-03-09 19:59:38 +0300

    task_1 completed

---
+++ lab_1 and lab_2/code/index.php
@@ -0,0 +1,10 @@
+<?php
+
+//task 1
+$very_bad_unclear_name = "15 chicken wings";
+// Write your code here:
+$order = &$very_bad_unclear_name;
+$order .= ' and pepsi';
+// Don't change the line below
+echo "\nYour order is: $very_bad_unclear_name.";
+echo "\n";
