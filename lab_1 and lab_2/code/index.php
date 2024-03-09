793a912fbf42652a4349f14b14b8957333afc7e1
solovey-88 <131691574+solovey-88@users.noreply.github.com>
2024-03-09 20:31:29 +0300

    task 2 completed

--- lab_1 and lab_2/code/index.php
+++ lab_1 and lab_2/code/index.php
@@ -1,10 +1,26 @@
 <?php
 
 //task 1
+echo "TASK 1", "<br />";
 $very_bad_unclear_name = "15 chicken wings";
 // Write your code here:
 $order = &$very_bad_unclear_name;
 $order .= ' and pepsi';
 // Don't change the line below
-echo "\nYour order is: $very_bad_unclear_name.";
-echo "\n";
+echo "\nYour order is: $very_bad_unclear_name.", "<br />", "<br />";
+
+
+//task 2
+echo "TASK 2", "<br />";
+$number1 = 5;
+echo $number1, "<br />";
+
+$number2 = 5.25;
+echo $number2, "<br />";
+
+echo 1000 - 988, "<br />";
+
+
+$last_month = 1187.23;
+$this_month = 1089.98;
+echo $this_month - $last_month, "<br />", "<br />";
