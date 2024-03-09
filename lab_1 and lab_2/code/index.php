cd6f3a2490681c60200d2a4aadeacbea42a98255
solovey-88 <131691574+solovey-88@users.noreply.github.com>
2024-03-09 21:49:16 +0300

    task 14 completed

--- lab_1 and lab_2/code/index.php
+++ lab_1 and lab_2/code/index.php
@@ -55,6 +55,95 @@ $answer -= $my_num;
 echo $answer, "<br />", "<br />";
 
 //task 14
+echo "TASK 14", "<br />";
 
+$a = 10;
+$b = 3;
+echo $a % $b, "<br />";
 
+if ($a % $b == 0)
+{
+    echo "Делится", $a / $b, "<br />";
+
+}
+else
+{
+    echo "Делится с остатком: ", $a % $b, "<br />";
+}
+
+$st = 2**10;
+echo $st, "<br />";
+echo sqrt(245), "<br />";
+
+$myFirstArray = [4, 2, 5, 19, 13, 0, 10];
+$sum = 0;
+foreach ($myFirstArray as $value)
+{
+    $sum += pow($value, 2);
+}
+echo sqrt($sum), "<br />";
+
+echo round(sqrt(379)), "<br />";
+echo round((sqrt(379)), 1), "<br />";
+echo round((sqrt(379)), 2), "<br />";
+
+$sqrt587 = sqrt(587);
+$resultArray = ['floor' => floor($sqrt587), 'ceil' => ceil($sqrt587)];
+echo $resultArray['floor'], "<br />";
+echo $resultArray['ceil'], "<br />";
+
+$mySecondArray = [4, -2, 5, 19, -130, 0, 10];
+echo min($mySecondArray), "<br />";
+echo max($mySecondArray), "<br />";
+
+echo rand(1, 100), "<br />";
+
+$randomArray = [];
+for ($i = 0; $i < 10; $i++)
+{
+    $randomArray[] = rand(1, 100);
+}
+foreach ($randomArray as $value)
+{
+    echo $value . " ";
+}
+echo "<br />";
+
+echo abs($a - $b), "<br />";
+$myThirdArray = [1, 2, -1, -2, 3, -3];
+$absoluteArray = [];
+foreach ($myThirdArray as $value)
+{
+    $absoluteArray[] = abs($value);
+}
+foreach ($absoluteArray as $value)
+{
+    echo $value . " ";
+}
+echo "<br />";
+
+$number30 = 30;
+$divisionsArray = [];
+for ($i = 1; $i <= $number30; $i++)
+{
+    if ($number30 % $i == 0)
+    {
+        $divisionsArray[] = $i;
+    }
+}
+foreach ($divisionsArray as $value)
+{
+    echo $value . " ";
+}
+echo "<br />";
+
+$myFourthArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+$sumForFourthArray = 0;
+$numberOfElement = 0;
+while ($sumForFourthArray <= 10)
+{
+    $sumForFourthArray += $myFourthArray[$numberOfElement];
+    $numberOfElement += 1;
+}
+echo $numberOfElement, "<br />", "<br />";
 
