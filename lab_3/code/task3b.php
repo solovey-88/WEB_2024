6df6edd9177777abf959aeb9908e12e397a98323
solovey-88 <131691574+solovey-88@users.noreply.github.com>
2024-03-30 09:29:54 +0200

    fixed lab_3

--- lab_3/code/task3b.php
+++ lab_3/code/task3b.php
@@ -8,14 +8,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
     $text = $_POST["text"];
 }
 
-$dir = "laba_3/$category";
+$dir = "categories/$category";
 if (!is_dir($dir)) {
     mkdir($dir, 0755, true);
 }
 
 
-$fileName = "laba_3/$category/{$title}.txt";
-$data = "Email: $email\nCategory: $category\nTitle: $title\nText: $text\n";
+$fileName = "categories/$category/{$title}.txt";
+$data = "$email \n$category \n$title \n$text \n";
 $write = fopen($fileName, "w");
 if ($write)
 {
